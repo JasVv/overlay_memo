4f25f7065bc030d4b26a394bf193cd7c6a3bacd1
JasVv <justice1322.zetta@gmail.com>
2022-09-13 12:56:00 +0900

    vue cli導入

--- script/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld('myApp', {
-  openJson: async () => ipcRenderer.invoke("openJson"),
-  updateTransparent: (callback) => ipcRenderer.on('update-transparent', callback),
-  updateContents: (id) => ipcRenderer.on('update-contents', id),
-});
--- script/setting_preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld('myAppSetting', {
-  openJson: async () => ipcRenderer.invoke("openJson"),
-  saveJson: async (text) => ipcRenderer.send("saveJson", text),
-  changeTransparent: (value) => ipcRenderer.send('change-transparent', value),
-  changeContents: (id) => ipcRenderer.send('change-contents', id),
-});
